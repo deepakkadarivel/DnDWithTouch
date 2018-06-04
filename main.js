1140b3cf16aeb4fa245046fd8527f1e5f8112c7c
deepakkadarivel <deepak.kadarivel@gmail.com>
2018-06-04 16:59:00 +0530

    001 drag start and touch start event for all objects

--- main.js
+++ main.js
@@ -11,8 +11,17 @@ function drag_start(event) {
 }
 window.onload = function () {
     var object1 = _('object1');
+    var object2 = _('object2');
+    var object3 = _('object3');
+
     object1.addEventListener('dragstart', handleDragStart, false);
     object1.addEventListener('touchstart', handleTouchStart, false);
+
+    object2.addEventListener('dragstart', handleDragStart, false);
+    object2.addEventListener('touchstart', handleTouchStart, false);
+
+    object3.addEventListener('dragstart', handleDragStart, false);
+    object3.addEventListener('touchstart', handleTouchStart, false);
 
     function handleDragStart(e) {
         _('app_status').innerHTML = "Dragging the element " + e
